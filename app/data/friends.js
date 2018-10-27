e1e0ce0041e2ee6b1f032e01259a2eafddbdbeb9
Ethan <ethan.holston@gmail.com>
2018-10-27 10:00:58 -0400

    api setup, survey questions stored

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendArr = [
+  {
+    name: "Ethan",
+    photo: "https://vignette.wikia.nocookie.net/missionimpossible/images/8/8f/Ethan_Hunt_III.jpg/revision/latest?cb=20150323091637",
+    answers: [1, 3, 5, 1, 3, 5, 1, 3, 5, 3]
+  }
+]
+
+module.exports = friendArr;
+
