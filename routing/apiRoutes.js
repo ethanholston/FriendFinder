8a9bd2ba377e4f8343567831f804445e1b21db11
Ethan <ethan.holston@gmail.com>
2018-10-29 19:43:29 -0400

    repushing w/ homepage

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -7,6 +7,28 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
+    var diffArr = [];
+    for(let i=0; i<friends.length; i++){
+      var diff=0;
+      for(let j=0; j<friends.answers; j++){
+        if (req.body.answers > friends[i].answers){
+          diff += (req.body.answers - friends[i].answers);
+        }
+        else{
+          diff += (friends[i].answers - req.body.answers)
+        }
+      }
+      diffArr.push(diff);
+    }
+    var lowestDiff = 50;
+    var bestFriend;
+    for(let k=0; k<diffArr.length; k++){
+      if(diffArr[k] < lowestDiff) {
+        lowestDiff = diffArr[k];
+        bestFriend = k;
+      }
+    }
+    res.json(friends[bestFriend]);
     friends.push(req.body);
   });
 }
